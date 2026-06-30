43735dfca80f0cb61c4690000790296cb24b532a
jeffersongoncalves <gerson.simao.92@gmail.com>
2026-06-30 18:39:27 +0000

    1.x: backport relation managers to Filament v3

--- src/Resources/StoreCreditAccount/Tables/StoreCreditAccountTable.php
+++ src/Resources/StoreCreditAccount/Tables/StoreCreditAccountTable.php
@@ -2,7 +2,7 @@
 
 namespace JeffersonGoncalves\FilamentCommerce\StoreCredit\Resources\StoreCreditAccount\Tables;
 
-use Filament\Actions;
+use Filament\Tables\Actions;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -18,11 +18,11 @@ class StoreCreditAccountTable
                 TextColumn::make('currency_code')
                     ->toggleable(),
             ])
-            ->recordActions([
+            ->actions([
                 Actions\EditAction::make(),
                 Actions\DeleteAction::make(),
             ])
-            ->toolbarActions([
+            ->bulkActions([
                 Actions\BulkActionGroup::make([
                     Actions\DeleteBulkAction::make(),
                 ]),
